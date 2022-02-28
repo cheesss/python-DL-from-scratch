a5e227ca9f981b5db3c3d565c52e5e421818ce7e
cheesss <90661454+cheesss@users.noreply.github.com>
2022-02-28 23:31:59 +0900

    .

--- pyqt_size_change.py
+++ pyqt_size_change.py
@@ -4,12 +4,9 @@ from PyQt5.QtWidgets import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        #내장함수 super를 이용한다.
-
+        self.setGeometry(100, 200, 300, 400)
 
 app = QApplication(sys.argv)
 window = MyWindow()
 window.show()
 app.exec_()
-
-# x를 누를때까지 창이 꺼지지 않도록 해준다.
