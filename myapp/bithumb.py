fa5653405ac5bbcb5c105a4d52eb1da7f4530d35
cheesss <90661454+cheesss@users.noreply.github.com>
2022-03-01 02:26:38 +0900

    .

--- myapp/bithumb.py
+++ myapp/bithumb.py
@@ -1,4 +1,5 @@
 import pybithumb
+import time
 
 con_key= "3588f3caa5b757bb8071c9a2bd4443c6"
 sec_key= "2cff6acf43cf1f68ee7fcecedbc1b588"
@@ -6,4 +7,28 @@ sec_key= "2cff6acf43cf1f68ee7fcecedbc1b588"
 bithumb = pybithumb.Bithumb(con_key,sec_key)
 
 balance = bithumb.get_balance("BTC")
-print(balance)
+
+# while True:
+#     price = pybithumb.get_current_price("BTC")
+#     print(price)
+#     time.sleep(0.2)
+# 비트코인 가격 0.2초마다 호출하기
+
+df = pybithumb.get_ohlcv("BTC")
+# # print(df.tail())
+# yesterday = df.iloc[-2]
+# today_open = yesterday['close']
+# yesterday_high = yesterday['high']
+# yesterday_low = yesterday['low']
+# target = today_open + (yesterday_high - yesterday_low) * 0.5
+# print(target)
+
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    yesterday = df.iloc[-2]
+
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
