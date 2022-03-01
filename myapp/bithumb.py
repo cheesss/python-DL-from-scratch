dbdbafa47b3efe1408c889268d2969a6a344ed88
cheesss <90661454+cheesss@users.noreply.github.com>
2022-03-01 11:25:37 +0900

    .

--- myapp/bithumb.py
+++ myapp/bithumb.py
@@ -1,5 +1,14 @@
-import pybihtumb
+import pybithumb
+import datetime
+import time
 
+with open("bithumb_api.txt") as f:
+    lines = f.readlines()
+    key = lines[0].strip()
+    secret = lines[1].strip()
+    bithumb = pybithumb.Bithumb(key, secret)
+
+#돌파전략 목표가 설정하기
 def get_target_price(ticker):
     df = pybithumb.get_ohlcv(ticker)
     yesterday = df.iloc[-2]
@@ -9,3 +18,38 @@ def get_target_price(ticker):
     yesterday_low = yesterday['low']
     target = today_open + (yesterday_high - yesterday_low) * 0.5
     return target
+
+# 자정에 목표가 갱신하기
+now = datetime.datetime.now()
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+target_price = get_target_price("BTC")
+
+def buy_crypto_currency(ticker):
+    if current_price > target_price:
+        krw = bithumb.get_balance(ticker)[2]
+        orderbook = pybithumb.get_orderbook(ticker)
+        sell_price = orderbook['asks'][0]['price']
+        unit = krw / float(sell_price)
+        bithumb.buy_market_order(ticker, unit)
+
+def sell_crypto_currency(ticker):
+    unit = bithumb.get_balance(ticker)[0]
+    bithumb.sell_market_order(ticker, unit)
+
+
+while True:
+    try:
+        now = datetime.datetime.now()
+        if mid < now < mid + datetime.timedelta(seconds=10):
+            target_price = get_target_price("BTC")
+            mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+            sell_crypto_currency("BTC")
+
+        current_price = pybithumb.get_current_price("BTC")
+        if current_price > target_price:
+            buy_crypto_currency("BTC")
+    except:
+        print("에러 발생")
+    time.sleep(1)
+
+
