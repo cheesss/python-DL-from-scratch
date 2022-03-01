9fc033bd8cd1626c41f9c427c78102d5feae91d9
cheesss <90661454+cheesss@users.noreply.github.com>
2022-03-01 10:57:16 +0900

    .

---
+++ myapp/bithumb.py
@@ -0,0 +1,11 @@
+import pybihtumb
+
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    yesterday = df.iloc[-2]
+
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
